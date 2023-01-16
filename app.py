4fdd34764ea2bfc38fcde368f63f1cc4e1b4dcc4
Rushabh Doshi <radoshi@gmail.com>
2023-01-15 16:49:47 -0800

    Everything works. Styles are rudimentary.

--- app.py
+++ app.py
@@ -3,8 +3,26 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
 from pathlib import Path
+from fastcore.all import *
+from fastai.vision.all import *
 
 app = Flask(__name__)
+
+db = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+)
+
+uploads = Path(app.instance_path)/'uploads'
+models = Path('models')
+data = Path('data')
+
+dls = db.dataloaders(data, bs=32)
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.load('trained')
 
 @app.route('/')
 def hello_world():
@@ -16,14 +34,17 @@ def check():
     # get the file from the post request
     file = request.files['file']
 
+    dec_pred, pred, probs = learn.predict(PILImage.create(file.stream))
+    print(f'{dec_pred}, {pred}, {probs}')    
+
     # save the file to the uploads folder
-    uploads = Path(app.instance_path)/'uploads'
-    if not uploads.exists():
-        os.makedirs(uploads)
+    # if not uploads.exists():
+    #     os.makedirs(uploads)
     
-    filename = uploads/secure_filename(file.filename)
-    file.save(filename)
+    # filename = uploads/secure_filename(file.filename)
+    # file.save(filename)
 
-    print(f'File saved to {filename}')
-    return render_template('result.html')
-
+    # print(f'File saved to {filename}')
+    prediction = dec_pred.upper()
+    probability = f'{(100 * probs[pred].item()):.02f}%'
+    return render_template('result.html', prediction=prediction, probability=probability)
